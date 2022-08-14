7d84b5eece2197b14edc6fdc72099e05e6f0207a
Erfan <75574558+ErfanAhangari@users.noreply.github.com>
2022-08-14 13:32:54 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function Copy() {
+  var Url = document.getElementById("url");
+  Url.innerHTML = window.location.href;
+  console.log(Url.innerHTML)
+  Url.select();
+  document.execCommand("copy");
+}
